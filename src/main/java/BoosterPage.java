980f4c5cb16561731df33c7139bf250dcb6b5e03
BenceZambo <bence.zambo96@gmail.com>
2017-11-13 13:30:13 +0100

    app upgraded

--- src/main/java/BoosterPage.java
+++ src/main/java/BoosterPage.java
@@ -9,8 +9,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import static com.sun.org.apache.xml.internal.serializer.Version.getProduct;
-import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoosterPage {
 
@@ -37,13 +37,10 @@ public class BoosterPage {
         commentColumn.setMinWidth(100);
         commentColumn.setCellValueFactory(new PropertyValueFactory<>("comments"));
 
-        //Button column
-        TableColumn<Button> buttonColumn = new TableColumn<>("Launch");
-        buttonColumn.setMinWidth(100);
-        buttonColumn.setCellValueFactory(new PropertyValueFactory<>("launch"));
+
 
         table = new TableView<>();
-        table.setItems(getOrders());
+        table.setItems(initData());
         table.getColumns().addAll(idColumn, priceColumn, purchaseColumn, commentColumn);
 
         Button launchButton = new Button("Launch");
@@ -59,17 +56,26 @@ public class BoosterPage {
         window.show();
     }
 
-    public ObservableList<Order> getOrders(){
+    public ObservableList<Order> initData(){
         ObservableList<Order> products = FXCollections.observableArrayList();
-        products.add(new Order(1, 859.00, "gold to dia", "pls"));
-        products.add(new Order(2, 2.49, "silver to dia", "ne bazd el"));
-        products.add(new Order(3, 99.00, "bronze to silver", "je"));
-        products.add(new Order(4, 19.99, "gold to master", ""));
-        products.add(new Order(5, 1.49, "master to challenger", "ez main"));
+        products.add(new Order(1, 859.00, "gold to dia", "pls", "asdf", "password1"));
+        products.add(new Order(2, 2.49, "silver to dia", "ne bazd el", "laci", "password2"));
+        products.add(new Order(3, 99.00, "bronze to silver", "je", "Laci", "password3"));
+        products.add(new Order(4, 19.99, "gold to master", "", "peti", "password4"));
+        products.add(new Order(5, 1.49, "master to challenger", "ez main", "répa", "password5"));
         return products;
     }
 
     public void launchButtonClicked(){
-        System.out.println("Launch lol.exe");
+        Order orderSelected;
+        List<String> accountData = new ArrayList<>();
+
+        orderSelected = table.getSelectionModel().getSelectedItem();
+
+        String username = orderSelected.getUsername();
+        String password = orderSelected.getPassword();
+
+        accountData.add(username);
+        accountData.add(password);
     }
 }
